53c39436607051e27c019d6561025b0e4667c141
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-07-05 23:41:33 -0700

    Refactor code to accept multiple components.

---
+++ app/task-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'task-list',
+  template: `
+  <ul>
+  <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
+  </ul>
+  `
+})
+
+export class TaskListComponent {
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonHasBeenClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not done. Better get to work!");
+    }
+  }
+
+}
